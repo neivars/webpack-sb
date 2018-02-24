07c456c5cc7fbb7d3ee9194c809b918c80ea4afb
Raul Neiva <raulsneiva@gmail.com>
2018-02-24 16:28:17 +0000

    End of guide before cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Packo from './components/my-component/packo-the-webpack-pack.png';
+import Data from './components/my-component/data.xml';
 
 function component() {
 	let element = document.createElement('div');
 
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	// Add the image to our existing div
+	let myImage = new Image();
+	myImage.src = Packo;
+
+	element.appendChild(myImage);
+
+	console.log(Data);
 
 	return element;
 }
