5cd4111eecbbc3bc9f5e3bf9d479d334c886de4c
Raul Neiva <raulsneiva@gmail.com>
2018-02-25 02:16:20 +0000

    End of guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,20 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
 		app: './src/index.js',
-		another: './src/another-module.js'
 	},
 	plugins: [
 		new CleanWebpackPlugin('dist'),
 		new HtmlWebpackPlugin({
 			title: 'Code Splitting'
 		}),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'common' // Specify the common bundle's name.
-		}),
 	],
 	output: {
 		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 };
